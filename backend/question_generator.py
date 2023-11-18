ceab994396511f34d486c45bb19d5cad9d1f103c
Network Saphyra <networksaphyra004@gmail.com>
2023-11-18 17:53:20 -0500

    Added server functionality, changed named from main.py to server.py

--- backend/question_generator.py
+++ backend/question_generator.py
@@ -1,12 +1,12 @@
 import openai
 import json
-import constants
+import ai_config
 
-class Questions:
+class QuestionGenerator:
     def __init__(self) -> None:
-        self.client = openai.OpenAI(api_key=constants.API_KEY)
+        self.client = openai.OpenAI(api_key=ai_config.API_KEY)
 
-    def _filter_multiple_choice_response(self, response):
+    def _extract_multiple_choice_data(self, response):
         filtered_response = response.choices[0].message.content
         data_dict = json.loads(filtered_response)
         question = data_dict.get("Question", "")
@@ -17,20 +17,20 @@ class Questions:
         filtered_array = [question, options, answer, explanation]
         return filtered_array
 
-    def create_multiple_choice_question(self, topic_query: str):
+    def generate_multiple_choice_question(self, topic_query: str):
         messages = [
-            {"role": "system", "content": constants.MULITPLE_CHOICE_SYSTEM_BEHAVIOR_PROMPT},
+            {"role": "system", "content": ai_config.MULITPLE_CHOICE_SYSTEM_BEHAVIOR_PROMPT},
             {"role": "user", "content": topic_query}
         ]
         response = self.client.chat.completions.create(
-            model = constants.MODEL,
+            model = ai_config.MODEL,
             messages = messages,
-            temperature = constants.MULTIPLE_CHOICE_TEMPERATURE,
-            max_tokens = constants.MULTIPLE_CHOICE_MAX_TOKENS
+            temperature = ai_config.MULTIPLE_CHOICE_TEMPERATURE,
+            max_tokens = ai_config.MULTIPLE_CHOICE_MAX_TOKENS
         ) 
-        return self._filter_multiple_choice_response(response)
+        return self._extract_multiple_choice_data(response)
 
-    def _filter_short_answer_response(self, response):
+    def _extract_short_answer_data(self, response):
         filtered_response = response.choices[0].message.content
         data_dict = json.loads(filtered_response)
         question = data_dict.get("Question", "")
@@ -40,20 +40,20 @@ class Questions:
         filtered_array = [question, answer, explanation]
         return filtered_array
      
-    def create_short_answer_question(self, topic_query: str):
+    def generate_short_answer_question(self, topic_query: str):
         messages = [
-            {"role": "system", "content": constants.SHORT_ANSWER_SYSTEM_BEHAVIOR_PROMPT},
+            {"role": "system", "content": ai_config.SHORT_ANSWER_SYSTEM_BEHAVIOR_PROMPT},
             {"role": "user", "content": topic_query}
         ]
         response = self.client.chat.completions.create(
-            model = constants.MODEL,
+            model = ai_config.MODEL,
             messages = messages,
-            temperature = constants.SHORT_ANSWER_TEMPERATURE,
-            max_tokens = constants.SHORT_ANSWER_MAX_TOKENS
+            temperature = ai_config.SHORT_ANSWER_TEMPERATURE,
+            max_tokens = ai_config.SHORT_ANSWER_MAX_TOKENS
         ) 
-        return self._filter_short_answer_response(response)
+        return self._extract_short_answer_data(response)
 
-    def _filter_true_or_false_response(self, response):
+    def _extract_true_or_false_data(self, response):
         filtered_response = response.choices[0].message.content
         data_dict = json.loads(filtered_response)
         question = data_dict.get("Question", "")
@@ -62,15 +62,15 @@ class Questions:
         filtered_array = [question, answer]
         return filtered_array
      
-    def create_true_or_false_question(self, topic_query: str):
+    def generate_true_or_false_question(self, topic_query: str):
         messages = [
-            {"role": "system", "content": constants.TRUE_OR_FALSE_SYSTEM_BEHAVIOR_PROMPT},
+            {"role": "system", "content": ai_config.TRUE_OR_FALSE_SYSTEM_BEHAVIOR_PROMPT},
             {"role": "user", "content": topic_query}
         ]
         response = self.client.chat.completions.create(
-            model = constants.MODEL,
+            model = ai_config.MODEL,
             messages = messages,
-            temperature = constants.TRUE_OR_FALSE_TEMPERATURE,
-            max_tokens = constants.TRUE_OR_FALSE_MAX_TOKENS
+            temperature = ai_config.TRUE_OR_FALSE_TEMPERATURE,
+            max_tokens = ai_config.TRUE_OR_FALSE_MAX_TOKENS
         ) 
-        return self._filter_true_or_false_response(response)
+        return self._extract_true_or_false_data(response)
---
+++ backend/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from question_generator import Questions
+import ai_config
+import openai
+
+app = Flask(__name__)
+openai.api_key = ai_config.API_KEY
+question_creator = Questions()
+
+@app.route("/members", methods=["GET"])
+def get_preferences():
+    client_data = request.json
+    topic, multiple_choices_num, short_answer_num, true_or_false_num = client_data
+    generated_questions = []
+
+    for _ in range(multiple_choices_num):
+        generated_questions.append(question_creator.create_multiple_choice_question(topic))
+    for _ in range(short_answer_num):
+        generated_questions.append(question_creator.create_short_answer_question(topic))
+    for _ in range(true_or_false_num):
+        generated_questions.append(question_creator.create_true_or_false_question(topic))
+
+    return {'generated_questions': generated_questions}, 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
