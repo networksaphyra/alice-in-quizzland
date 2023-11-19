e65142f4c836216ddef84d1861c43672d8a97049
Network Saphyra <networksaphyra004@gmail.com>
2023-11-19 14:03:39 -0500

    Frontend designing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => {
     return <div className='Home'>
-        <h1>Quiz Maker</h1>
+        <header>
+            <h1>Welcome to the Wonderland of Quizzes!</h1>
+        </header>
         <img className="logo" src="/logo.png" alt="logo" />
         <h2>Spend Less Time Finding Questions.</h2>
         <Link to="/quiz">
-        <Button className="start" href='/quiz'>Start Now!</Button>
+        <Button className="start" href='/quiz'>Click To Knock!</Button>
         </Link>
     </div>
 }
