f44a84910598d8de12e8b1e542f4355f68477df2
Network Saphyra <networksaphyra004@gmail.com>
2023-11-18 19:36:57 -0500

    Added opening image to root directory

--- backend/question_generator.py
+++ backend/question_generator.py
@@ -74,3 +74,23 @@ class QuestionGenerator:
             max_tokens = ai_config.TRUE_OR_FALSE_MAX_TOKENS
         ) 
         return self._extract_true_or_false_data(response)
+    
+if __name__ == "__main__":
+    generator = QuestionGenerator()
+
+    # Test generate_multiple_choice_question
+    topic_query = "The Roman Empire"
+    generated_multiple_choice = generator.generate_multiple_choice_question(topic_query)
+    # print("Generated Multiple Choice Question:")
+    # print(generated_multiple_choice, end="\n\n")
+
+    # # Test generate_short_answer_question
+    # generated_short_answer = generator.generate_short_answer_question(topic_query)
+    # print("Generated Short Answer Question:")
+    # print(generated_short_answer, end="\n\n")
+
+    # Test generate_true_or_false_question
+    generated_true_or_false = generator.generate_true_or_false_question(topic_query)
+    print("Generated True or False Question:")
+    print(generated_true_or_false, end="\n\n")
+ 
--- backend/server.py
+++ backend/server.py
@@ -23,5 +23,7 @@ def get_preferences() -> dict:
 
     return {'generated_questions': generated_questions}, 200
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
