2b2289f52626287da9bb53b61706aef05d7a2c80
Network Saphyra <networksaphyra004@gmail.com>
2023-11-18 21:57:23 -0500

    Added Brute-Force to eliminate invalid JSON formats

--- backend/ai_config.py
+++ backend/ai_config.py
@@ -5,11 +5,11 @@ MODEL = "gpt-3.5-turbo-1106"
 MULTIPLE_CHOICE_TEMPERATURE = 0.1
 MULTIPLE_CHOICE_MAX_TOKENS = 500
 MULITPLE_CHOICE_OPTION_NUMBER = 4
-MULTIPLE_CHOICE_QUESTION_NUM = 5
+MULTIPLE_CHOICE_QUESTION_NUM = 1
 MULITPLE_CHOICE_SYSTEM_BEHAVIOR_PROMPT=f"""
     Generate {MULTIPLE_CHOICE_QUESTION_NUM} question and multiple-choice options based on the provided topic. 
     Return a perfectly working and valid JSON Array with Objects inside it.
-    The array and objects have the following format:
+    The array and objects should have the following format:
     [
     {{
         "Question": "The question",
@@ -17,41 +17,38 @@ MULITPLE_CHOICE_SYSTEM_BEHAVIOR_PROMPT=f"""
         "Answer": "Correct option for the question",
         "Explanation": "A brief explanation for the answer for this question",
     }},
-    ... repeat with {MULTIPLE_CHOICE_QUESTION_NUM} different questions
     ]
 """
 
 # Short Answers
 SHORT_ANSWER_TEMPERATURE = 0.5
 SHORT_ANSWER_MAX_TOKENS = 500
-SHORT_ANSWER_QUESTION_NUM = 5
+SHORT_ANSWER_QUESTION_NUM = 1
 SHORT_ANSWER_SYSTEM_BEHAVIOR_PROMPT=f"""
     Generate {SHORT_ANSWER_QUESTION_NUM} short question and a short answer on the provided topic. 
     Return a perfectly working and valid JSON Array with Objects inside it.
-    The array and objects have the following format:
+    The array and objects should have the following format:
     [
     {{
-        "Question": "The question"
-        "Answer": "Short answer to the question"
-        "Explanation": "A brief explanation for the answer for this question"
-        }},
-    ... repeat with {SHORT_ANSWER_QUESTION_NUM} different questions
+        "Question": "The question", 
+        "Answer": "Short answer to the question",
+        "Explanation": "A brief explanation for the answer for this question",
+    }},
     ]
 """
 
 # True or False
 TRUE_OR_FALSE_TEMPERATURE = 0.1
 TRUE_OR_FALSE_MAX_TOKENS = 50
-TRUE_OR_FALSE_QUESTION_NUM = 5
+TRUE_OR_FALSE_QUESTION_NUM = 1
 TRUE_OR_FALSE_SYSTEM_BEHAVIOR_PROMPT=f"""
     Generate {TRUE_OR_FALSE_QUESTION_NUM} statements that can only be true or false. Example: Questions: "The Earth is Round", Answer: True. Instead of some generic question such as: Question: "What shape is the earth?"
     Return a perfectly working and valid JSON Array with Objects inside it.
-    The array and objects have the following format:
+    The array and objects should have the following format:
     [
     {{
-        "Question": "The question"
+        "Question": "The question",
         "Answer": "True or false answer to that question"
     }},
-    ... repeat with {TRUE_OR_FALSE_QUESTION_NUM} different questions
     ]
 """
--- backend/question_generator.py
+++ backend/question_generator.py
@@ -8,8 +8,11 @@ class QuestionGenerator:
 
     def _extract_multiple_choice_data(self, response):
         filtered_response = response.choices[0].message.content
-        data_dict = json.loads(filtered_response)
-
+        try: 
+            data_dict = json.loads(filtered_response)
+        except json.JSONDecodeError:
+            print("Error Handled...")
+            return None
         return data_dict
 
     def generate_multiple_choice_question(self, topic_query: str):
@@ -27,41 +30,56 @@ class QuestionGenerator:
 
     def _extract_short_answer_data(self, response):
         filtered_response = response.choices[0].message.content
-        data_dict = json.loads(filtered_response)
-
+        try: 
+            data_dict = json.loads(filtered_response)
+        except json.JSONDecodeError:
+            print("Error Handled...")
+            return None
         return data_dict
      
     def generate_short_answer_question(self, topic_query: str):
-        messages = [
-            {"role": "system", "content": ai_config.SHORT_ANSWER_SYSTEM_BEHAVIOR_PROMPT},
-            {"role": "user", "content": topic_query}
-        ]
-        response = self.client.chat.completions.create(
-            model = ai_config.MODEL,
-            messages = messages,
-            temperature = ai_config.SHORT_ANSWER_TEMPERATURE,
-            max_tokens = ai_config.SHORT_ANSWER_MAX_TOKENS
-        ) 
-        return self._extract_short_answer_data(response)
+        while True:
+            messages = [
+                {"role": "system", "content": ai_config.SHORT_ANSWER_SYSTEM_BEHAVIOR_PROMPT},
+                {"role": "user", "content": topic_query}
+            ]
+            response = self.client.chat.completions.create(
+                model = ai_config.MODEL,
+                messages = messages,
+                temperature = ai_config.SHORT_ANSWER_TEMPERATURE,
+                max_tokens = ai_config.SHORT_ANSWER_MAX_TOKENS
+            ) 
+            sucessful = self._extract_short_answer_data(response)
+            if (sucessful): 
+                break
+        return sucessful
+
 
     def _extract_true_or_false_data(self, response):
         filtered_response = response.choices[0].message.content
-        data_dict = json.loads(filtered_response)
-
+        try:
+            data_dict = json.loads(filtered_response)
+        except json.JSONDecodeError:
+            print("Error Handled...")
+            return None
         return data_dict
      
     def generate_true_or_false_question(self, topic_query: str):
-        messages = [
-            {"role": "system", "content": ai_config.TRUE_OR_FALSE_SYSTEM_BEHAVIOR_PROMPT},
-            {"role": "user", "content": topic_query}
-        ]
-        response = self.client.chat.completions.create(
-            model = ai_config.MODEL,
-            messages = messages,
-            temperature = ai_config.TRUE_OR_FALSE_TEMPERATURE,
-            max_tokens = ai_config.TRUE_OR_FALSE_MAX_TOKENS
-        ) 
-        return self._extract_true_or_false_data(response)
+        while True:
+            messages = [
+                {"role": "system", "content": ai_config.TRUE_OR_FALSE_SYSTEM_BEHAVIOR_PROMPT},
+                {"role": "user", "content": topic_query}
+            ]
+            response = self.client.chat.completions.create(
+                model = ai_config.MODEL,
+                messages = messages,
+                temperature = ai_config.TRUE_OR_FALSE_TEMPERATURE,
+                max_tokens = ai_config.TRUE_OR_FALSE_MAX_TOKENS
+            ) 
+            successful = self._extract_multiple_choice_data(response)
+            if successful:
+                break
+        return successful
     
 if __name__ == "__main__":
     generator = QuestionGenerator()
