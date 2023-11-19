2ffaa93d63c35c59b36623f9808b95af1738a659
Network Saphyra <networksaphyra004@gmail.com>
2023-11-18 23:14:53 -0500

    Organized and made object-oriented

--- backend/server.py
+++ backend/server.py
@@ -3,28 +3,46 @@ from question_generator import QuestionGenerator
 import ai_config
 import openai
 
-app = Flask(__name__)
-openai.api_key = ai_config.API_KEY
-question_creator = QuestionGenerator()
+class RequestHandler:
+    def __init__(self):
+        self.app = Flask(__name__)
+        openai.api_key = ai_config.API_KEY
+        self.question_generator = QuestionGenerator()
 
-@app.route("/members", methods=["POST"])
-def get_preferences() -> dict:
-    client_data = request.json
-    topic = client_data["topic"]
-    multiple_choices_num, short_answer_num, true_or_false_num = client_data["multipleChoice"], client_data["shortAnswer"], client_data["trueOrFalse"]
-    ai_config.MULTIPLE_CHOICE_QUESTION_NUM, ai_config.SHORT_ANSWER_QUESTION_NUM, ai_config.TRUE_OR_FALSE_QUESTION_NUM = multiple_choices_num, short_answer_num, true_or_false_num
-    print(multiple_choices_num, short_answer_num, true_or_false_num)
-    generated_questions = []
+    def send_generated_questions(self, client_data) -> dict:
+        topic = client_data["topic"]
+        multiple_choice_num = client_data["multipleChoice"]
+        short_answer_num = client_data["shortAnswer"]
+        true_or_false_num = client_data["trueOrFalse"]
 
-    generated_questions.append(question_creator.generate_multiple_choice_question(topic))
-    print('here')
-    generated_questions.append(question_creator.generate_short_answer_question(topic))
-    print('here')
-    generated_questions.append(question_creator.generate_true_or_false_question(topic))
-    print('here')
-    return {'generated_questions': generated_questions}, 200
+        ai_config.MULTIPLE_CHOICE_QUESTION_NUM = multiple_choice_num
+        ai_config.SHORT_ANSWER_QUESTION_NUM = short_answer_num
+        ai_config.TRUE_OR_FALSE_QUESTION_NUM = true_or_false_num
 
+        generated_questions = []
+        generated_questions.append(self.question_generator.generate_multiple_choice_question(topic))
+        generated_questions.append(self.question_generator.generate_short_answer_question(topic))
+        generated_questions.append(self.question_generator.generate_true_or_false_question(topic))
+        
+        return {'generated_questions': generated_questions}, 200
 
+    def check_short_answer(self, client_data) -> dict:
+        pass
+
+    def handle_requests(self) -> dict:
+        client_data = request.json
+        if client_data["check"]:
+            return self.check_short_answer(client_data=client_data)
+        else:
+            return self.send_generated_questions(client_data=client_data)
+
+    def run_server(self):
+        @self.app.route("/members", methods=["POST"])
+        def handle_requests_wrapper():
+            return self.handle_requests()
+
+        self.app.run(debug=True)
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    handler = RequestHandler()
+    handler.run_server()
