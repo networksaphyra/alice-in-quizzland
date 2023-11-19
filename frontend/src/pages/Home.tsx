b8019df496d95ceb78b266494d87f0d96bb4176a
Network Saphyra <networksaphyra004@gmail.com>
2023-11-19 15:13:43 -0500

    Design

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import { Button } from '@mui/joy'
 import { Link } from 'react-router-dom'
+import '../Home.css';
 
 export const Home = () => {
-    return <div className='Home'>
-        <header>
-            <h1>Welcome to the Wonderland of Quizzes!</h1>
-        </header>
-        <img className="logo" src="/logo.png" alt="logo" />
-        <h2>Spend Less Time Finding Questions.</h2>
-        <Link to="/quiz">
-        <Button className="start" href='/quiz'>Click To Knock!</Button>
-        </Link>
-    </div>
+    return (
+        <div className='Home'>
+            <header>
+                <h1>Welcome to the Wonderland of Quizzes!</h1>
+            </header>
+            <img className="logo" src="/logo.png" alt="logo" />
+            <h2>Spend Less Time Finding Questions.</h2>
+            <Link to="/quiz">
+                <Button className="start">Click To Knock!</Button>
+            </Link>
+        </div>
+    );
 }
